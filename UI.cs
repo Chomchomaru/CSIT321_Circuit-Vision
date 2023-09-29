442e956d568c9d2318862037426ce8e521c77407
Vipul Vikraman <81799106+Lupiv3D@users.noreply.github.com>
2023-09-29 13:24:59 +0400

    Update UI.cs

--- UI.cs
+++ UI.cs
@@ -1,1 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+
+    public static UI Instance;
+
+    private int currentPage = 0;
+    public GameObject BG;
+    public GameObject [] pages;
+
+    private void Awake()
+    {
+        if (Instance != null) Destroy(gameObject);
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*Screen.orientation = ScreenOrientation.Portrait;*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangePage(int pageNum)
+    {
+        pages[currentPage].SetActive(false);
+        pages[pageNum].SetActive(true);
+        currentPage = pageNum;
+        if (currentPage == 4) BG.SetActive(false);
+    }
+
+    /*public void ToProject()
+    {
+        BG.SetActive(false);
+        ChangePage(3);
+    }*/
+}
 
